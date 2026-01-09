1ab7d148e470556dc9105f9cdd5753ec87777f07
Kona <conradulysses@outlook.com>
2026-01-08 20:09:41 -0800

    made it not run through terminal

--- script/manifest-generator.py
+++ script/manifest-generator.py
@@ -4,6 +4,7 @@ import yaml
 
 banned_flags_list = ['a','b','B','c','C','D','E','F','g','G','h','H','L','N','P','t','W','x','Y']
 
+# Example YAML entry:
 #   gen:
 #     name: Gender Factor
 #     abbr: gen
@@ -14,26 +15,20 @@ banned_flags_list = ['a','b','B','c','C','D','E','F','g','G','h','H','L','N','P'
 #     is_flag: true
 #     flag: g
 
-def add_entry():
-    yaml.YAMLObject
+# example flag entry:
+# ["gen", "Gender Factor", 'Numerical', -100, 100, 0, True, "g"]
+# [Abbreviation, Name, Type, Min, Max, Default, is_flag?, flag]
 
-def make_flag():
-    flag_abbreviation = input("What is the abbreviation of the flag?")
-    flag_name = input("What is the full name of the flag?")
-    type_number = input("What type of flag is it? 1=Bool, 2=option, 3=Numerical, 4=Curve")
-    if type_number == 1:
-        print("Selected: Boolean")
+flags = [
+    [],
+    []
+]
 
-    elif type_number == 2:
-        print("Selected: Option")
-    elif type_number == 3:
-        print("Selected: Numerical")
-    elif type_number == 4:
-        print("Selected: Curve")
-    else:
-        print("invalid type; starting over </3")
-        make_flag()
+resampler_name = 'TIPS'
 
-def __init__():
-    exe_name = input("Please input the name of the resampler (without file extension)")
+
+
+
+
+
 
